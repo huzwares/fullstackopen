01b090e7fcedc11d4e36c6a15dbfec98cc435415
Sina M. Omrani <sina.omrani@hotmail.com>
2022-08-27 23:19:09 +0430

    2.11: The Phonebook Step6. (also add refreshPage function to set persons and search together. I not sure how to do this efficiently)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Filter = ({ search }) => {
 	return (
@@ -35,15 +36,22 @@ const Persons = (props) => {
 }
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
-		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-	])
+	const [persons, setPersons] = useState([])
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
 	const [showSearch, setShowSearch] = useState(persons)
+
+	const refreshPage = (data) => {
+		setPersons(data)
+		setShowSearch(data)
+	}
+
+	useEffect(() => {
+		axios.get('http://localhost:3001/persons').then(response => {
+			refreshPage(response.data)
+		})
+	}, [])
+
 	const handleNameChange = (event) => {
 		setNewName(event.target.value)
 	}
@@ -61,8 +69,7 @@ const App = () => {
 				number: newNumber,
 				id: persons.at(-1).id + 1
 			}
-			setPersons(persons.concat(nameObject))
-			setShowSearch(persons.concat(nameObject))
+			refreshPage(persons.concat(nameObject))
 			setNewNumber('')
 			setNewName('')
 		} else {
