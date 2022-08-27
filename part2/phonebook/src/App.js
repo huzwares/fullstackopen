862828141b27978beb2a649950241b1fe9fc9b07
Sina M. Omrani <sina.omrani@hotmail.com>
2022-08-27 18:56:25 +0430

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,34 @@
 import { useState } from 'react';
 
 const App = () => {
-	const [persons, setPersons] = useState(
-		[{ name: 'arto Hellas', number: '040-123456' }]
-	)
+	const [persons, setPersons] = useState([
+		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
+		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+	])
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
-
+	const [showSearch, setShowSearch] = useState(persons)
 	const handleNameChange = (event) => {
 		setNewName(event.target.value)
 	}
 	const handleNumberChange = (event) => {
 		setNewNumber(event.target.value)
 	}
+	const handleSearch = (event) => {
+		setShowSearch(persons.filter(person => person.name.toLowerCase().includes(event.target.value.toLowerCase())))
+	}
 	const addNewName = (event) => {
 		event.preventDefault()
 		if (!persons.map(person => person.name).includes(newName)) {
 			const nameObject = {
 				name: newName,
-				number: newNumber
+				number: newNumber,
+				id: persons.at(-1).id + 1
 			}
 			setPersons(persons.concat(nameObject))
+			setNewNumber('')
 			setNewName('')
 		} else {
 			alert(`${newName} is already added to phonebook`)
@@ -30,6 +38,8 @@ const App = () => {
 	return (
 		<div>
 			<h2>Phonebook</h2>
+			<p>filter shown with <input onChange={handleSearch} /></p>
+			<h2>add a new</h2>
 			<form onSubmit={addNewName}>
 				<div>
 					name: <input value={newName} onChange={handleNameChange} />
@@ -42,7 +52,7 @@ const App = () => {
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			{persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+			{showSearch.map(person => <p key={person.id}>{person.name} {person.number}</p>)}
 		</div>
 	)
 }
