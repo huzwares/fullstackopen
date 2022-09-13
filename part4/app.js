c30307e72780ba50c62d6ab255d019cda4ca29b8
Sina M. Omrani <sina.omrani@hotmail.com>
2022-09-13 20:42:37 +0430

    4.15: bloglist expansion, step3

--- part4/app.js
+++ part4/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const userRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -16,5 +17,6 @@ mongoose.connect(config.MONGODB_URI).then(() => {
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', userRouter)
 
 module.exports = app
